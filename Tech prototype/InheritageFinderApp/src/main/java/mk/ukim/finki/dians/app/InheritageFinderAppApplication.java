c9cc8571bf743267daec0b8b36fcfc6aa497e688
Emilija13 <89068860+Emilija13@users.noreply.github.com>
2023-12-03 16:47:42 +0100

    Add files via upload

---
+++ Tech prototype/InheritageFinderApp/src/main/java/mk/ukim/finki/dians/app/InheritageFinderAppApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.dians.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InheritageFinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InheritageFinderAppApplication.class, args);
+	}
+
+}
